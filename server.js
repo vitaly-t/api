ad679454eda82ec6a5f79a91b27cc71c792bfb21
Alanna Scott <alanna.scott@gmail.com>
2019-04-06 19:03:54 -0700

    force port 3001

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ require("dotenv").config({ silent: process.env.NODE_ENV === "production" });
 let http = require("http");
 let app = require("./app");
 
-http.createServer(app).listen(app.get("port"), function() {
-  console.log("Express server listening on port %s", app.get("port"));
+http.createServer(app).listen(3001, function() {
+  console.log("Express server listening on port %s", 3001);
 });
